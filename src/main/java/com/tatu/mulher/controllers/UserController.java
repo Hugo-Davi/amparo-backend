f576ae084aa15f92fdbd13edc84c9f386f5939c6
Hugo-Davi <hugodavisilvadeandrade@gmail.com>
2024-09-19 21:37:46 -0300

    Testando armazenamento na API

--- src/main/java/com/tatu/mulher/controllers/UserController.java
+++ src/main/java/com/tatu/mulher/controllers/UserController.java
@@ -1,8 +1,6 @@
 package com.tatu.mulher.controllers;
 
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -11,14 +9,23 @@ import org.springframework.web.bind.annotation.PathVariable;
 @RequestMapping(value = "/user")
 public class UserController {
 
+    private long idArmazenado;
+
     @RequestMapping(value = "/", headers = "key=teste", method = RequestMethod.GET)
     public String getUsers(){
         System.out.println("users get");
         return "funcionando";
     }
+
+    @RequestMapping(value = "/valor", method = RequestMethod.GET)
+    public String getIdArmazenado(){
+        return "Id Armazenado no server é: " + this.idArmazenado;
+    }
+
     @RequestMapping(value = "/{id}", method = RequestMethod.GET)
     public String getUserById (@PathVariable("id") long id){
         System.out.println(id);
+        this.idArmazenado = id;
         return "O id desse usuário é: " + id;
     }
 }
