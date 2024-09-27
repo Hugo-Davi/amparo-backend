212be5ca3763884550ca6982f326911eee5cc07d
Hugo-Davi <hugodavisilvadeandrade@gmail.com>
2024-09-27 09:13:52 -0300

    Class Response

--- src/main/java/com/tatu/mulher/controllers/UserController.java
+++ src/main/java/com/tatu/mulher/controllers/UserController.java
@@ -3,6 +3,7 @@ package com.tatu.mulher.controllers;
 import com.google.gson.Gson;
 import com.tatu.mulher.models.User;
 import com.tatu.mulher.services.UserService;
+import com.tatu.mulher.utils.Response;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
@@ -20,53 +21,49 @@ public class UserController {
 
     @RequestMapping(value = "/", method = RequestMethod.GET)
     public ResponseEntity<String> getUsers(){
-        String json;
-        json = new Gson().toJson(this.userService.getAllUsers());
-        return ResponseEntity.ok(json);
+        String response = new Gson().toJson(this.userService.getAllUsers());
+        return ResponseEntity.ok(response);
     }
 
     @RequestMapping(value = "/{id}", method = RequestMethod.GET)
     public ResponseEntity<String> getUserById(@PathVariable("id") String id) {
-        String json;
-
-        json = new Gson().toJson(this.userService.getUser(id));
-
-        return ResponseEntity.ok(json);
+        String response = new Gson().toJson(this.userService.getUser(id));
+        return ResponseEntity.ok(response);
     }
 
     @RequestMapping(value = "/", method = RequestMethod.POST, produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<String> createUser (@RequestBody User user){
         this.userService.saveUser(user);
-        return ResponseEntity.ok("{status:funcionou}");
+        Response response = new Response("Usuário foi salvo");
+        return ResponseEntity.ok(new Gson().toJson(response));
     }
 
     @RequestMapping(value = "/", method = RequestMethod.PUT, produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<String> updateUser (@RequestBody User user){
-        String json;
-
+        // Checa se ID existe na estrutura passada
         if (user.getId() == null){
-            json = "message: ID não fornecido";
+            Response response = new Response("ID não fornecido");
             return ResponseEntity
-                    .unprocessableEntity().body(new Gson().toJson(json));
+                    .unprocessableEntity().body(new Gson().toJson(response));
         }
+
         this.userService.saveUser(user);
-        json = "message: criado";
+
+        Response response = new Response("Usuário atualizado");
         return ResponseEntity
-                .ok(new Gson().toJson(json));
+                .ok(new Gson().toJson(response));
     }
 
     @RequestMapping(value = "/{id}", method = RequestMethod.DELETE)
     public ResponseEntity<String> updateUser(@PathVariable String id) {
-        String json;
-
         User user = this.userService.getUser(id);
         if (user == null) {
             return ResponseEntity.notFound().build();
         }
         this.userService.deleteUser(id);
-        json = new Gson().toJson(user);
+        String response = new Gson().toJson(user);
 
-        return ResponseEntity.ok(json);
+        return ResponseEntity.ok(response);
     }
 
 }
