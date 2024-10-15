9b5979fc473aac1bdb5bcdcc70262853be680ac9
Hugo-Davi <hugodavisilvadeandrade@gmail.com>
2024-10-15 20:42:44 -0300

    JWT encoder and decoder with nimbus jose

--- src/main/java/com/tatu/mulher/config/SecurityConfig.java
+++ src/main/java/com/tatu/mulher/config/SecurityConfig.java
@@ -1,5 +1,10 @@
 package com.tatu.mulher.config;
 
+import com.nimbusds.jose.jwk.JWK;
+import com.nimbusds.jose.jwk.JWKSet;
+import com.nimbusds.jose.jwk.RSAKey;
+import com.nimbusds.jose.jwk.source.ImmutableJWKSet;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.annotation.Order;
@@ -12,8 +17,15 @@ import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.oauth2.jwt.JwtDecoder;
+import org.springframework.security.oauth2.jwt.JwtEncoder;
+import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
+import org.springframework.security.oauth2.jwt.NimbusJwtEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+
+import java.security.interfaces.RSAPrivateKey;
+import java.security.interfaces.RSAPublicKey;
 
 import static org.springframework.security.config.Customizer.withDefaults;
 
@@ -21,9 +33,14 @@ import static org.springframework.security.config.Customizer.withDefaults;
 @EnableWebSecurity
 public class SecurityConfig {
 
+    @Value("${jwt.public.key}")
+    private RSAPublicKey publicKey;
+    @Value("${jwt.private.key}")
+    private RSAPrivateKey privateKey;
+
     @Bean
     // authentication
-    public UserDetailsService userDetailsService(PasswordEncoder encoder){
+    private UserDetailsService userDetailsService(PasswordEncoder encoder){
         User.UserBuilder users = User.withDefaultPasswordEncoder();
         InMemoryUserDetailsManager manager = new InMemoryUserDetailsManager();
         manager.createUser(users.username("user").password("password").roles("USER").build());
@@ -33,7 +50,7 @@ public class SecurityConfig {
 
 //    @Bean
 //    @Order(1)
-//    public SecurityFilterChain apiFilterChain(HttpSecurity http) throws Exception {
+//    private SecurityFilterChain apiFilterChain(HttpSecurity http) throws Exception {
 //        http
 //                .securityMatcher("/api/**")
 //                .authorizeHttpRequests(authorize -> authorize
@@ -44,7 +61,7 @@ public class SecurityConfig {
 //    }
 
     @Bean
-    public SecurityFilterChain formLoginFilterChain(HttpSecurity http) throws Exception {
+    private SecurityFilterChain formLoginFilterChain(HttpSecurity http) throws Exception {
         http
                 .authorizeHttpRequests(authorize -> authorize
                         .anyRequest().authenticated()
@@ -54,6 +71,18 @@ public class SecurityConfig {
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
         return http.build();
     }
+
+    @Bean
+    public JwtDecoder jwtDecoder() {
+        return NimbusJwtDecoder.withPublicKey(publicKey).build();
+    }
+
+    @Bean JwtEncoder jwtEncoder() {
+        JWK jwk = new RSAKey.Builder(this.publicKey).privateKey(privateKey).build();
+        var jwks = new ImmutableJWKSet<>(new JWKSet(jwk));
+        return new NimbusJwtEncoder(jwks);
+    }
+
 
     @Bean
     public PasswordEncoder passwordEncoder () {
--- src/main/java/com/tatu/mulher/services/JwtService.java
+++ src/main/java/com/tatu/mulher/services/JwtService.java
@@ -4,6 +4,7 @@ import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
+import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Component;
 
 import java.security.Key;
@@ -14,25 +15,26 @@ import java.util.Map;
 @Component
 public class JwtService {
 
-    public static final String SECRET = "AbrnosrmBF9Kka5LGLs3GcHjOKqWi1g0lYlk32YEp7K5W9tZqr2x3Xlz";
 
-    public String generateToken(String userName){
-        Map<String, Object> claims = new HashMap<>();
-        return createToken(claims, userName);
-    }
-
-    private String createToken(Map<String, Object> claims, String userName) {
-        return Jwts.builder()
-                .setClaims(claims)
-                .setSubject(userName)
-                .setIssuedAt(new Date(System.currentTimeMillis()))
-                .setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * 30))
-                .signWith(getSignKey(), SignatureAlgorithm.HS256).compact();
-    }
-
-    private Key getSignKey() {
-        byte[] keyBytes = Decoders.BASE64.decode(SECRET);
-        return Keys.hmacShaKeyFor(keyBytes);
-    }
+//    public static final String SECRET = "AbrnosrmBF9Kka5LGLs3GcHjOKqWi1g0lYlk32YEp7K5W9tZqr2x3Xlz";
+//
+//    public String generateToken(String userName){
+//        Map<String, Object> claims = new HashMap<>();
+//        return createToken(claims, userName);
+//    }
+//
+//    private String createToken(Map<String, Object> claims, String userName) {
+//        return Jwts.builder()
+//                .setClaims(claims)
+//                .setSubject(userName)
+//                .setIssuedAt(new Date(System.currentTimeMillis()))
+//                .setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * 30))
+//                .signWith(getSignKey(), SignatureAlgorithm.HS256).compact();
+//    }
+//
+//    private Key getSignKey() {
+//        byte[] keyBytes = Decoders.BASE64.decode(SECRET);
+//        return Keys.hmacShaKeyFor(keyBytes);
+//    }
 
 }
