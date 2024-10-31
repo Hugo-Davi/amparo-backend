131d5a90f6789f92730f8f382b74329049422ec1
Hugo-Davi <hugodavisilvadeandrade@gmail.com>
2024-10-31 14:48:44 -0300

    auth SCOPE

--- src/main/java/com/tatu/amparo/controllers/PostController.java
+++ src/main/java/com/tatu/amparo/controllers/PostController.java
@@ -10,6 +10,8 @@ import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @RestController
 @RequestMapping(value = "/post")
 public class PostController {
@@ -18,23 +20,22 @@ public class PostController {
     private PostService service;
 
     @RequestMapping(value = "/", method = RequestMethod.GET)
-    public ResponseEntity<String> getAll (){
-        String response = new Gson().toJson(this.service.getAll());
-        return ResponseEntity.ok(response);
+    public ResponseEntity<List<Post>> getAll (){
+        return ResponseEntity.ok(this.service.getAll());
     }
+
     @RequestMapping(value = "/{id}", method = RequestMethod.GET)
-    public ResponseEntity<String> getById(@PathVariable("id") String id) {
-        String response = new Gson().toJson(this.service.get(id));
-        return ResponseEntity.ok(response);
+    public ResponseEntity<Post> getById(@PathVariable("id") String id) {
+        return ResponseEntity.ok(this.service.get(id));
     }
+
     @RequestMapping(value = "/", method = RequestMethod.POST, produces = MediaType.APPLICATION_JSON_VALUE)
-    public ResponseEntity<String> create (@RequestBody Post post){
-        this.service.save(post);
-        Response response = new Response("Post foi salvo");
-        return ResponseEntity.ok(new Gson().toJson(response));
+    public ResponseEntity<Post> create (@RequestBody Post post){
+        return ResponseEntity.ok(this.service.save(post));
     }
+
     @RequestMapping(value = "/{id}", method = RequestMethod.PUT, produces = MediaType.APPLICATION_JSON_VALUE)
-    public ResponseEntity<String> update (@PathVariable("id") String id, @RequestBody Post post){
+    public ResponseEntity<Post> update (@PathVariable("id") String id, @RequestBody Post post){
 
         if (!this.service.existById(id)){
             return ResponseEntity.notFound().build();
@@ -44,25 +45,22 @@ public class PostController {
 
         Response response = new Response("Usuário atualizado");
         return ResponseEntity
-                .ok(new Gson().toJson(response));
+                .ok(post);
     }
     @RequestMapping(value = "/{id}", method = RequestMethod.DELETE)
-    public ResponseEntity<String> delete(@PathVariable String id) {
+    public ResponseEntity<Post> delete(@PathVariable String id) {
         Post post = this.service.get(id);
         if (post == null) {
             return ResponseEntity.notFound().build();
         }
         this.service.delete(id);
-        String response = new Gson().toJson(post);
-
         return ResponseEntity
-                .ok(response);
+                .ok(post);
     }
 
     @RequestMapping(value = "/{id}/comment", method = RequestMethod.POST, produces = MediaType.APPLICATION_JSON_VALUE)
-    public ResponseEntity<String> createComment (@PathVariable String id, @RequestBody Comment comment){
+    public ResponseEntity<Comment> createComment (@PathVariable String id, @RequestBody Comment comment){
         this.service.postComment(id, comment);
-        Response response = new Response("Comentário adicionado");
-        return ResponseEntity.ok(new Gson().toJson(response));
+        return ResponseEntity.ok(comment);
     }
 }
--- src/main/java/com/tatu/amparo/controllers/UserController.java
+++ src/main/java/com/tatu/amparo/controllers/UserController.java
@@ -9,6 +9,7 @@ import com.tatu.amparo.utils.Response;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.oauth2.jwt.JwtClaimsSet;
@@ -26,27 +27,25 @@ public class UserController {
     @Autowired
     private UserService service;
 
+    @PreAuthorize("hasAuthority('SCOPE_ADMIN')")
     @RequestMapping(value = "/", method = RequestMethod.GET)
-    public ResponseEntity<String> getAll(){
-        String response = new Gson().toJson(this.service.getAll());
-        return ResponseEntity.ok(response);
+    public ResponseEntity<List<User>> getAll(){
+        return ResponseEntity.ok(this.service.getAll());
     }
 
     @RequestMapping(value = "/{id}", method = RequestMethod.GET)
-    public ResponseEntity<String> getById(@PathVariable("id") String id) {
-        String response = new Gson().toJson(this.service.get(id));
-        return ResponseEntity.ok(response);
+    public ResponseEntity<User> getById(@PathVariable("id") String id) {
+        return ResponseEntity.ok(this.service.get(id));
     }
 
+    @PreAuthorize("hasAuthority('SCOPE_ADMIN')")
     @RequestMapping(value = "/", method = RequestMethod.POST, produces = MediaType.APPLICATION_JSON_VALUE)
-    public ResponseEntity<String> create (@RequestBody User user){
-        this.service.save(user);
-        Response response = new Response("Usuário foi salvo");
-        return ResponseEntity.ok(new Gson().toJson(response));
+    public ResponseEntity<User> create (@RequestBody User user){
+        return ResponseEntity.ok(this.service.save(user));
     }
 
     @RequestMapping(value = "/{id}", method = RequestMethod.PUT, produces = MediaType.APPLICATION_JSON_VALUE)
-    public ResponseEntity<String> update (@PathVariable("id") String id, @RequestBody User user){
+    public ResponseEntity<User> update (@PathVariable("id") String id, @RequestBody User user){
 
         if (!this.service.existById(id)){
             return ResponseEntity.notFound().build();
@@ -54,22 +53,21 @@ public class UserController {
         user.setId(id);
         this.service.save(user);
 
-        Response response = new Response("Usuário atualizado");
         return ResponseEntity
-                .ok(new Gson().toJson(response));
+                .ok(user);
     }
 
+    @PreAuthorize("hasAuthority('SCOPE_ADMIN')")
     @RequestMapping(value = "/{id}", method = RequestMethod.DELETE)
-    public ResponseEntity<String> delete(@PathVariable String id) {
+    public ResponseEntity<User> delete(@PathVariable String id) {
         User user = this.service.get(id);
         if (user == null) {
             return ResponseEntity.notFound().build();
         }
         this.service.delete(id);
-        String response = new Gson().toJson(user);
 
         return ResponseEntity
-                .ok(response);
+                .ok(user);
     }
 
 }
